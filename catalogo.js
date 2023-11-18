c328a832f09964de1e7364bc0cd6360e420c873c
Marcela Hadassa <135720339+marcelahadassa@users.noreply.github.com>
2023-11-18 10:34:44 -0300

    v2

--- catalogo.js
+++ catalogo.js
@@ -1,18 +1,26 @@
-var listaLivros = [];
-listaLivros[0] =
-  "https://m.media-amazon.com/images/I/91LptBSFxQL._AC_UF1000,1000_QL80_.jpg";
-listaLivros[1] =
-  "https://m.media-amazon.com/images/I/71uY1Ce7d2L._AC_UF894,1000_QL80_.jpg";
-listaLivros[2] =
-  "https://cdn.awsli.com.br/800x800/2507/2507517/produto/2107069317d8bdbcc97.jpg";
-listaLivros[3] =
-  "https://m.media-amazon.com/images/I/71kImPVRpAL._AC_UF1000,1000_QL80_.jpg";
+var listaLivros = ["https://m.media-amazon.com/images/I/91LptBSFxQL._AC_UF1000,1000_QL80_.jpg",
+  "https://m.media-amazon.com/images/I/71uY1Ce7d2L._AC_UF894,1000_QL80_.jpg",
+  "https://cdn.awsli.com.br/800x800/2507/2507517/produto/2107069317d8bdbcc97.jpg",
+  "https://m.media-amazon.com/images/I/71kImPVRpAL._AC_UF1000,1000_QL80_.jpg"
+];
+var livrosNomes = ['Percy Jackson e o ladrão de raios', 'Alan Turing: The Enigma', 'Fazendo meu Filme 4', 'O clube dos amigos imaginários'];
 
-for (var i = 0; i < 4; i = i++) {
-  document.write("<img src= " + listaLivros[i] + ">");
+var livrosExistentes = [];
+
+function adicionarLivro() {
+  var addLivro = document.getElementById('bookURL').value;
+  var elementoListaLivros = document.getElementById('listaLivros');
+  elementoListaLivros.innerHTML = '<img src=' + addLivro + '>'
+  
 }
 
-document.write("<img src= " + listaLivros[0] + ">");
-document.write("<img src= " + listaLivros[1] + ">");
-document.write("<img src= " + listaLivros[2] + ">");
-document.write("<img src= " + listaLivros[3] + ">");
+
+for (var i = 0; i < listaLivros.length; i++) {
+
+
+  document.write("<div style='display: inline-block; margin-top: 20px; margin-right: 30px; text-align: center;'>");
+  document.write("<img src='" + listaLivros[i] + "'>");
+  document.write("<p style='color: #ffffff; font-family: Verdana, sans-serif;'>" + livrosNomes[i] + "</p>");
+  document.write("</div>");
+}
+
